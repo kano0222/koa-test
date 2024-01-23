66b50d37b6f1b064d2bf2a54f2f758ee1bb8bc16
kano0222 <870689576@qq.com>
2024-01-24 03:49:41 +0800

    连接数据库

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 const { PORT } = require('./config/config.default')
+const { addMongoDB } = require('./data')
 
-const app = require('./app')
-
-app.listen(PORT, () => {
-  console.log(`server is running on ${PORT}!`)
-})
+const cluster_1 = require("cluster")
+async function run () {
+  console.info(`Node版本：`, process.versions.node)
+  console.info(`NODE_ENV：`, process.env.NODE_ENV)
+  console.info(`创建开发主进程`)
+  cluster_1.on("listening", (worker, address) => {
+    console.log(`工作进程 ${worker.process.pid} 监听于 ${address.address}:${address.port}`)
+  })
+  cluster_1.on("exit", (worker, code, signal) => {
+    console.warn(`工作进程 ${worker.process.pid} 已退出，返回值 ${code} (${signal})`)
+    if (!code) {
+      cluster_1.default.fork()
+    }
+  })
+  const app = require('./app')
+  await addMongoDB(app)
+  app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}!`)
+  })
+}
+run()
+// db.createUser({ user: "kano0222", pwd: "kano224", roles: [{ role: 'root', db: "admin" }] })
